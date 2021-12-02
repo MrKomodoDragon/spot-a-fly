44f1e7093f4eec93dad0d7f0c2fad3d73c2e4d9b
MrKomodoDragon <svrchn921@outlook.com>
2021-12-01 17:12:56 -0800

    me when login page complete

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import asyncio
 from logging import exception
+import re
 from quart import Quart, request
+import quart
+from quart.helpers import url_for
+from quart.templating import render_template
+from quart.utils import redirect
 import quart_auth   # type: ignore
 import json
 import argon2
@@ -16,7 +21,7 @@ pool = loop.run_until_complete(
     )
 )
 app = Quart(
-    __name__,
+    __name__, template_folder='templates'
 )
 app.secret_key = __import__('secrets').token_urlsafe(16)
 quart_auth.AuthManager(app)
@@ -27,33 +32,32 @@ async def index():
     return "Spot-a-fly, the world's first app that lets you keep track of the flies you have spotted in your lifetime. Made by MrKomodoDragon and Jay3332."
 
 
-@app.route('/login', methods=['GET'])
-async def login():
-    data = request.args
-    password = data['password']
-    try:
-        ph = argon2.PasswordHasher()
-        hash = await pool.fetchval(
-            'SELECT password from users WHERE email = $1', data['email']
-        )
-        ph.verify(hash, password)
-        quart_auth.login_user(
-            quart_auth.AuthUser(
-                await pool.fetchval(
-                    'SELECT username from users WHERE email = $1',
-                    data['email'],
-                )
-            )
-        )
-        return 'Logged in'
-    except Exception as e:
-        return f'Authentication failed :(. Error: {e}\n'
+
+@app.route('/login', methods=['GET', 'POST'])
+async def login_():
+    if request.method == "POST":
+        data = await request.form
+        password = data['password']
+        try:
+            ph = argon2.PasswordHasher()
+            hash = await pool.fetchval('SELECT password from users WHERE email = $1', data['email'])
+            ph.verify(hash, password)
+            quart_auth.login_user(quart_auth.AuthUser(await pool.fetchval('SELECT username from users WHERE email = $1',data['email'],)))
+            return quart.redirect(url_for('home'))
+        except Exception as e:
+            return f'Authentication failed :(. Error: {e}\n'
+    return await render_template("login.html")
 
 
-@app.route('/username')
+@app.route('/home')
 @quart_auth.login_required
-async def username():
-    return quart_auth.current_user.auth_id
+async def home():
+    return f" Welcome, {quart_auth.current_user.auth_id}, to Spot-a-fly!"
+
+@app.route("/sus_login", methods=["POST"])
+async def sus_login():
+    print(await request.form)
+    return (await request.form)
 
 
 app.run(loop=loop)
